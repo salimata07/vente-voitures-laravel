10eeca9e613e3f6aa90cb5f91d2c9abba4c5bade
EtudiantA <salib2080@gmail.com>
2026-09-01 13:10:14 +0100

    Creation de la page d'accueil personnalisee

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CarController;
 
 Route::get('/', function () {
-    return view('welcome');
+    $latestCars = \App\Models\Car::latest()->take(6)->get();
+    return view('welcome', compact('latestCars'));
 });
 
 Auth::routes();
